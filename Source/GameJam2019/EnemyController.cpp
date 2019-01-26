793114fe7dae3c9783ad12800a3f4a7aa175b2a1
Jonas Andersen <20195223+NotBjoggisAtAll@users.noreply.github.com>
2019-01-26 11:34:26 +0100

    Moved enemy logic to player

--- Source/GameJam2019/EnemyController.cpp
+++ Source/GameJam2019/EnemyController.cpp
@@ -5,6 +5,7 @@
 #include "Engine/World.h"
 #include "Engine/Engine.h"
 #include "DrawDebugHelpers.h"
+#include "DogEnemy.h"
 
 AEnemyController::AEnemyController()
 {
@@ -16,50 +17,51 @@ void AEnemyController::BeginPlay()
 	Super::BeginPlay();
 	
 	HomeLocation = GetPawn()->GetActorLocation();
+	
 }
 
 void AEnemyController::Tick(float DeltaTime)
  {
 	 Super::Tick(DeltaTime);
 
-	 // create tarray for hit results
-	 TArray<FHitResult> OutHits;
+	// // create tarray for hit results
+	// TArray<FHitResult> OutHits;
 
-	 // start and end locations
-	 FVector SweepStart = GetPawn()->GetActorLocation();
-	 FVector SweepEnd = GetPawn()->GetActorLocation();
+	// // start and end locations
+	// FVector SweepStart = GetPawn()->GetActorLocation();
+	// FVector SweepEnd = GetPawn()->GetActorLocation();
 
-	 // create a collision sphere
-	 FCollisionShape MyColSphere = FCollisionShape::MakeSphere(1000.f);
+	// // create a collision sphere
+	// FCollisionShape MyColSphere = FCollisionShape::MakeSphere(1000.f);
 
-	 // draw collision sphere
-	 DrawDebugSphere(GetWorld(), GetPawn()->GetActorLocation(), MyColSphere.GetSphereRadius(), 50, FColor::Purple, true);
+	// // draw collision sphere
+	// DrawDebugSphere(GetWorld(), GetPawn()->GetActorLocation(), MyColSphere.GetSphereRadius(), 50, FColor::Purple, true);
 
-	 // check if something got hit in the sweep
-	 bool isHit = GetWorld()->SweepMultiByChannel(OutHits, SweepStart, SweepEnd, FQuat::Identity, ECC_GameTraceChannel1, MyColSphere);
+	// // check if something got hit in the sweep
+	//// bool isHit = GetWorld()->SweepMultiByChannel(OutHits, SweepStart, SweepEnd, FQuat::Identity, ECC_Pawn, MyColSphere);
+	// bool isHit = false;
+	// if (isHit)
+	// {
+	//
+	//	 // loop through TArray
+	//	 for (auto& Hit : OutHits)
+	//	 {
+	//		 Player = Cast<AKidPlayer>(Hit.Actor);
+	//		 if (Player)
+	//		 {
+	//			 LastPlayerLocation = Player->GetActorLocation();
+	//			 MoveToLocation(LastPlayerLocation, 600.f, true, true);
 
-	 if (isHit)
-	 {
-	
-		 // loop through TArray
-		 for (auto& Hit : OutHits)
-		 {
-			 Player = Cast<AKidPlayer>(Hit.Actor);
-			 if (Player)
-			 {
-				 LastPlayerLocation = Player->GetActorLocation();
-				 MoveToLocation(LastPlayerLocation, 600.f, true, true);
+	//			 GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Hit Result: %s"), *Player->GetName()));
 
-				 GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Hit Result: %s"), *Player->GetName()));
-
-			 }
-			 if (GEngine)
-			 {
-				 // screen log information on what was hit
-				 // uncommnet to see more info on sweeped actor
-				 // GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("All Hit Information: %s"), *Hit.ToString()));
-			 }
-		 }
-	 }
+	//		 }
+	//		 if (GEngine)
+	//		 {
+	//			 // screen log information on what was hit
+	//			 // uncommnet to see more info on sweeped actor
+	//			 // GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("All Hit Information: %s"), *Hit.ToString()));
+	//		 }
+	//	 }
+	// }
  }
 
